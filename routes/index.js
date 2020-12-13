3e1757a827033b9b1a553a5010a2fda9937e057e
Duarte <duarte.paradela@gmail.com>
2020-12-13 23:30:50 +0100

    Create and update are operational

--- routes/index.js
+++ routes/index.js
@@ -5,32 +5,41 @@ const uploader = require("./../config/cloudinary");
 const protectPrivateRoute = require("./../middlewares/protectPrivateRoute");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
   res.render("index", { title: "New Talent" });
 });
 
-router.get("/dashboard", function (req, res, next) {
+router.get("/dashboard", function (req, res) {
   res.render("dashboard");
 });
 
-router.get("/hotfive", function (req, res, next) {
+router.get("/hotfive", function (req, res) {
   res.render("hotFive");
 });
 
-router.get("/send-freestyle", function (req, res, next) {
+router.get("/send-freestyle", function (req, res) {
   res.render("sendFreestyle");
 });
 
+// router.post("/send-freestyle", async (req, res, next) => {
+//   try {
+//     newPost = { ...req.body };
+//     await PostModel.create(newPost);
+//     res.redirect("/");
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
 router.post(
   "/send-freestyle",
-  protectPrivateRoute,
   uploader.single("video"),
   async (req, res, next) => {
     const newPost = { ...req.body };
     if (!req.file) {
-      newPost.image = undefined;
+      newPost.video = undefined;
     } else {
-      newPost.image = req.file.path;
+      newPost.video = req.file.path;
     }
     try {
       await PostModel.create(newPost);
@@ -41,7 +50,7 @@ router.post(
   }
 );
 
-router.get("/publication/:id", protectPrivateRoute, async (req, res) => {
+router.get("/publication/:id", async (req, res) => {
   try {
     const post = await PostModel.findById(req.params.id);
     res.render("publication", post);
@@ -50,34 +59,35 @@ router.get("/publication/:id", protectPrivateRoute, async (req, res) => {
   }
 });
 
-router.get(
-  "/edit-freestyle/:id",
-  protectPrivateRoute,
-  async function (req, res, next) {
-    try {
-      const post = await PostModel.findById(req.params.id);
-      console.log(req.params.id);
-      res.render("editFreestyle", post);
-    } catch (err) {
-      next(err);
-    }
+router.get("/edit-freestyle/:id", async function (req, res, next) {
+  try {
+    const post = await PostModel.findById(req.params.id);
+    res.render("editFreestyle", post);
+  } catch (err) {
+    next(err);
   }
-);
+});
 
 router.post(
   "/edit-freestyle/:id",
-  protectPrivateRoute,
-  async function (req, res, next) {
+  uploader.single("video"),
+  async (req, res, next) => {
+    const editPost = { ...req.body };
+    if (!req.file) {
+      editPost.video = undefined;
+    } else {
+      editPost.video = req.file.path;
+    }
     try {
-      await PostModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
-      res.redirect("/dashboard");
+      await PostModel.findByIdAndUpdate(req.params.id, editPost, { new: true });
+      res.redirect("/");
     } catch (err) {
       next(err);
     }
   }
 );
 
-router.get("/delete/:id", protectPrivateRoute, async function (req, res, next) {
+router.get("/delete/:id", async function (req, res, next) {
   try {
     await PostModel.findByIdAndRemove(req.params.id);
     res.redirect("/dashboard");
